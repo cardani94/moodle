a0a88f417156d7e7687d913b5928db137c6f420f
Petr Skoda <commits@skodak.org>
2011-03-19 20:07:21 +0100

    fix version and block non-mysql dbs

--- version.php
+++ version.php
@@ -8,5 +8,5 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version   = 2011031900;
 $plugin->requires  = 2010100900;  // Requires this Moodle version
 $plugin->cron      = 0;
-$plugin->release   = '3.3.10';
+$plugin->release   = '3.3.10.0';
 $plugin->component = 'local_phpmyadmin';
