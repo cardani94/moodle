4d45685ab67f90b52202ab404bf06e6515ada4bf
Petr Skoda <commits@skodak.org>
2011-03-19 15:16:45 +0100

    not used in moodle phpmyadmin integration

--- test/classes/_data/gen_version_info/info.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Full infrmation about theme, including $theme_generation and $theme_version
- */
-$theme_name = 'Test Theme';
-$theme_generation = "2";
-$theme_version = "0.3";
--- test/classes/_data/incorrect_theme/info.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Incorrect theme information file.
- * Full name is not specified
- */
-
-$theme_full_version = '1.0';
