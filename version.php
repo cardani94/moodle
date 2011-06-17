74a9d886000fd71616f9361da93f1d98d30d9845
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-06-17 14:47:01 +0200

    Updated version to 3.3.10.1 (upto PMASA-2011-4)

--- version.php
+++ version.php
@@ -5,8 +5,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2011031900;
+$plugin->version   = 2011061700;
 $plugin->requires  = 2010100900;  // Requires this Moodle version
 $plugin->cron      = 0;
-$plugin->release   = '3.3.10.0';
+$plugin->release   = '3.3.10.1';
 $plugin->component = 'local_phpmyadmin';
