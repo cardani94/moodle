3ac9c3604ee8e101fa882161addbf118572beba7
Rajesh Taneja <rajesh@moodle.com>
2012-05-02 11:11:25 +0800

    MDL-30959 phpmyadmin: updated version, after applying security patches

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2011061700;
+$plugin->version   = 2012050200;
 $plugin->requires  = 2010100900;  // Requires this Moodle version
 $plugin->cron      = 0;
 $plugin->release   = '3.3.10.1';
