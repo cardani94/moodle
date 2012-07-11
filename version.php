7ec46bf0ece767dda2e512df85dac3bbdb2601d6
Rajesh Taneja <rajesh@moodle.com>
2012-07-11 15:44:14 +0800

    MDL-30798 phpmyadmin: Updated version to 3.5.2

--- version.php
+++ version.php
@@ -5,8 +5,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2011061700;
+$plugin->version   = 2012071100;
 $plugin->requires  = 2010100900;  // Requires this Moodle version
 $plugin->cron      = 0;
-$plugin->release   = '3.3.10.1';
+$plugin->release   = '3.5.2.0';
 $plugin->component = 'local_phpmyadmin';
