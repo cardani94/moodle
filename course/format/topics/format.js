84b4382c219a5211c8998ba47730b4a8212d82d8
Rajesh Taneja <rajesh@moodle.com>
2012-10-10 09:58:50 +0800

    MDL-33570 ajax: Check if both swap nodes are present

--- course/format/topics/format.js
+++ course/format/topics/format.js
@@ -41,7 +41,7 @@ M.course.format.swap_sections = function(Y, node1, node2) {
 
     var sectionlist = Y.Node.all('.'+CSS.COURSECONTENT+' '+M.course.format.get_section_selector(Y));
     // Swap menus if they exist.
-    if (sectionlist.item(node1).one('.'+CSS.SECTIONADDMENUS)) {
+    if (sectionlist.item(node1).one('.'+CSS.SECTIONADDMENUS) && sectionlist.item(node2).one('.'+CSS.SECTIONADDMENUS)) {
         sectionlist.item(node1).one('.'+CSS.SECTIONADDMENUS).swap(sectionlist.item(node2).one('.'+CSS.SECTIONADDMENUS));
     }
 }
