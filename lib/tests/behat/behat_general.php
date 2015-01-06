edd5e3c037acd7cf2e5a292914eb76c2cdf1718b
Rajesh Taneja <rajesh@moodle.com>
2015-01-06 15:20:29 +0800

    MDL-46891 behat: Updated xpath, as crawler update it
    DomCrawler\Crawler is making XPath relative to the current context,so use descendant
    to ensure it's not update by Crawler

--- lib/tests/behat/behat_general.php
+++ lib/tests/behat/behat_general.php
@@ -1081,7 +1081,9 @@ class behat_general extends behat_base {
 
         // Check if value exists in specific row/column.
         // Get row xpath.
-        $rowxpath = $tablexpath."/tbody/tr[th[normalize-space(.)=" . $rowliteral . "] | td[normalize-space(.)=" . $rowliteral . "]]";
+        // GoutteDriver uses DomCrawler\Crawler and it is making XPath relative to the current context, so use descendant.
+        $rowxpath = $tablexpath."/tbody/tr[descendant::th[normalize-space(.)=" . $rowliteral .
+                    "] | descendant::td[normalize-space(.)=" . $rowliteral . "]]";
 
         $columnvaluexpath = $rowxpath . $columnpositionxpath . "[contains(normalize-space(.)," . $valueliteral . ")]";
 
