96bb51d63d0af1367012b15bcc96a76f0c488f1a
Rajesh Taneja <rajesh@moodle.com>
2016-03-18 12:12:42 +0800

    MDL-53381 behat: Replaced chained steps with api.
    Behat 3.x doesn't support chained steps and hence
    this is first step to get rid of chained steps in core.

--- mod/glossary/tests/behat/behat_mod_glossary.php
+++ mod/glossary/tests/behat/behat_mod_glossary.php
@@ -27,8 +27,7 @@
 
 require_once(__DIR__ . '/../../../../lib/behat/behat_base.php');
 
-use Moodle\BehatExtension\Context\Step\Given as Given,
-    Behat\Gherkin\Node\TableNode as TableNode;
+use Behat\Gherkin\Node\TableNode as TableNode;
 
 /**
  * Glossary-related steps definitions.
@@ -47,11 +46,14 @@ class behat_mod_glossary extends behat_base {
      * @param TableNode $data
      */
     public function i_add_a_glossary_entry_with_the_following_data(TableNode $data) {
-        return array(
-            new Given('I press "' . get_string('addentry', 'mod_glossary') . '"'),
-            new Given('I set the following fields to these values:', $data),
-            new Given('I press "' . get_string('savechanges') . '"')
-        );
+        $this->execute("behat_forms::press_button", get_string('addentry', 'mod_glossary'),
+            true, true);
+
+        $this->execute("behat_forms::i_set_the_following_fields_to_these_values", $data,
+            true, true);
+
+        $this->execute("behat_forms::press_button", get_string('savechanges'),
+            false, false);
     }
 
     /**
@@ -62,13 +64,21 @@ class behat_mod_glossary extends behat_base {
      */
     public function i_add_a_glossary_entries_category_named($categoryname) {
 
-        return array(
-            new Given('I follow "' . get_string('categoryview', 'mod_glossary') . '"'),
-            new Given('I press "' . get_string('editcategories', 'mod_glossary') . '"'),
-            new Given('I press "' . get_string('add').' '.get_string('category', 'glossary') . '"'),
-            new Given('I set the field "name" to "' . $this->escape($categoryname) . '"'),
-            new Given('I press "' . get_string('savechanges') . '"'),
-            new Given('I press "' . get_string('back', 'mod_glossary') . '"')
-        );
+        $this->execute("behat_general::click_link", get_string('categoryview', 'mod_glossary'),
+            true, true);
+
+        $this->execute("behat_forms::press_button", get_string('editcategories', 'mod_glossary'),
+            true, true);
+
+        $this->execute("behat_forms::press_button", get_string('add').' '.get_string('category', 'glossary'),
+            true, true);
+
+        $this->execute('behat_forms::i_set_the_field_to', array('name', $this->escape($categoryname)),
+            false, false);
+
+        $this->execute("behat_forms::press_button", get_string('savechanges'),
+            true, true);
+        $this->execute("behat_forms::press_button", get_string('back', 'mod_glossary'),
+            false, false);
     }
 }
